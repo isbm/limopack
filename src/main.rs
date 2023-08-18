7f063d3279d224e9b01534f2c1ac171a8b6ecbb0
Bo Maryniuk <bo@maryniuk.net>
2023-08-18 14:43:54 +0200

    Add initial CLI options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::{Arg, Command, ArgAction};
+use std::env;
+
+fn cli() -> Command<'static> {
+    Command::new("linmodpak - Linux Module Package helper")
+        .arg(Arg::new("tree").short('e').long("tree").help("Display module dependency tree").action(ArgAction::SetTrue))
+        .arg(Arg::new("debug").short('d').long("debug").help("Set to debug mode").action(ArgAction::SetTrue))
+        .arg(Arg::new("version").short('v').long("version").help("Get current version").action(ArgAction::SetTrue))
+    }
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        let _ = cli().print_help();
+        return;
+    }
+
+    let params = cli().get_matches();
+    if params.get_flag("tree") {
+        println!("Tree!");
+    }
 }
