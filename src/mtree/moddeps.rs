bc9c9d76ebcc092c8bbc0ea1dd04e46b7fd80102
Bo Maryniuk <bo@maryniuk.net>
2023-08-22 20:44:08 +0200

    Add module output sorter

--- src/mtree/moddeps.rs
+++ src/mtree/moddeps.rs
@@ -1,5 +1,6 @@
 pub mod ktree {
     use crate::mtree::kerman::kman::KernelInfo;
+    use crate::mdb::modules::modinfo;
     use std::collections::HashMap;
 
     pub struct KModuleTree<'kinfo> {
@@ -11,16 +12,39 @@ pub mod ktree {
             KModuleTree { kernel: kinfo }
         }
 
-        pub fn load(mut self) -> Self {
-            self
+        /// lsmod
+        fn get_loaded_modules(&self) -> Vec<String> {
+            let mut deps: Vec<String> = vec![];
+            deps
         }
 
-        pub fn get_tree(&self) {}
-
-        /*
-        pub fn get_deplist(&self) -> HashMap<String, Vec<String>> {
-            return self.deplist.clone();
+        /// Snapshot currently active modules (lsmod)
+        pub fn get_loaded(&self) -> HashMap<String, Vec<String>> {
+            self.get_specified(&self.get_loaded_modules())
         }
-        */
+
+        /// Get all dependencies for the specified modules
+        pub fn get_specified(&self, modules: &[String]) -> HashMap<String, Vec<String>> {
+            self.kernel.get_deps_for(modules)
+        }
+
+        /// Same as a snapshot `get_loaded()` except it is merges
+        /// all the dependencies into one list for an actual operations.
+        pub fn merge_loaded(&self) -> Vec<String> {
+            self.merge_specified(&self.get_loaded_modules())
+        }
+
+        /// Same as `get_specified` method, except it merges
+        /// all the dependencies into one list for an actual operations.
+        pub fn merge_specified(&self, modules: &[String]) -> Vec<String> {
+            let mut deps: Vec<String> = vec![];
+            for (module, data) in self.get_specified(modules) {
+                deps.extend(data);
+                deps.push(module);
+            }
+
+            deps.sort();
+            deps
+        }
     }
 }
